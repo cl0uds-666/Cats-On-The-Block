7eefda4aaa71e7fca346e642144c7f06a1ed84ab
Adam-Bolton-23827742 <adam.bolton23@my.northampton.ac.uk>
2024-10-08 10:32:33 +0100

    started working on ability to move between different cover

--- Assets/Scripts/Adam/Cover.cs
+++ Assets/Scripts/Adam/Cover.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Cover : MonoBehaviour
@@ -12,6 +13,8 @@ public class Cover : MonoBehaviour
     public float HighCast;
     public float LowCast;
     bool RunCouroutine = false;
+    public float DetectionRadius;
+    private GameObject SecondaryCoverHit;
     void Start()
     {
         
@@ -60,12 +63,28 @@ public class Cover : MonoBehaviour
                 {
                     GetComponent<Rigidbody>().linearVelocity = new Vector3(0, GetComponent<Rigidbody>().linearVelocity.y, GetComponent<Rigidbody>().linearVelocity.z);
                     transform.position = new Vector3(EdgeDetection.transform.position.x - 0.5f * CoverHit.transform.localScale.x, transform.position.y, transform.position.z);
+
+                    if (Physics.SphereCast(transform.position, DetectionRadius, Vector3.one, out RaycastHit HitInfo))
+                    {
+                        if (HitInfo.collider.gameObject != CoverHit && HitInfo.transform.position.x < transform.position.x)
+                        {
+                            SecondaryCoverHit = HitInfo.collider.gameObject;
+                        }
+                    }
                 }
 
                 else if (transform.position.x >= EdgeDetection.transform.position.x + 0.5f * CoverHit.transform.localScale.x)
                 {
                     GetComponent<Rigidbody>().linearVelocity = new Vector3(0, GetComponent<Rigidbody>().linearVelocity.y, GetComponent<Rigidbody>().linearVelocity.z);
                     transform.position = new Vector3(EdgeDetection.transform.position.x + 0.5f * CoverHit.transform.localScale.x, transform.position.y, transform.position.z);
+
+                    if (Physics.SphereCast(transform.position, DetectionRadius, Vector3.one, out RaycastHit HitInfo))
+                    {
+                        if (HitInfo.collider.gameObject != CoverHit && HitInfo.transform.position.x > transform.position.x)
+                        {
+                            SecondaryCoverHit = HitInfo.collider.gameObject;
+                        }
+                    }
                 }
 
                 GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotation | RigidbodyConstraints.FreezePositionZ | RigidbodyConstraints.FreezePositionY;
@@ -77,12 +96,28 @@ public class Cover : MonoBehaviour
                 {
                     GetComponent<Rigidbody>().linearVelocity = new Vector3(GetComponent<Rigidbody>().linearVelocity.x, GetComponent<Rigidbody>().linearVelocity.y, 0);
                     transform.position = new Vector3(transform.position.x, transform.position.y, EdgeDetection.transform.position.z - 0.5f * CoverHit.transform.localScale.z);
+
+                    if (Physics.SphereCast(transform.position, DetectionRadius, Vector3.one, out RaycastHit HitInfo))
+                    {
+                        if (HitInfo.collider.gameObject != CoverHit && HitInfo.transform.position.z < transform.position.z)
+                        {
+                            SecondaryCoverHit = HitInfo.collider.gameObject;
+                        }
+                    }
                 }
 
                 else if (transform.position.z >= EdgeDetection.transform.position.z + 0.5f * CoverHit.transform.localScale.z)
                 {
                     GetComponent<Rigidbody>().linearVelocity = new Vector3(GetComponent<Rigidbody>().linearVelocity.x, GetComponent<Rigidbody>().linearVelocity.y, 0);
                     transform.position = new Vector3(transform.position.x, transform.position.y, EdgeDetection.transform.position.z + 0.5f * CoverHit.transform.localScale.z);
+
+                    if (Physics.SphereCast(transform.position, DetectionRadius, Vector3.one, out RaycastHit HitInfo))
+                    {
+                        if (HitInfo.collider.gameObject != CoverHit && HitInfo.transform.position.z > transform.position.z)
+                        {
+                            SecondaryCoverHit = HitInfo.collider.gameObject;
+                        }
+                    }
                 }
 
                 GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotation | RigidbodyConstraints.FreezePositionX | RigidbodyConstraints.FreezePositionY;
@@ -147,5 +182,13 @@ public class Cover : MonoBehaviour
 
         yield return new WaitForSeconds(0.5f);
         RunCouroutine = false;
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (InCover)
+        {
+            Gizmos.DrawWireSphere(transform.position, DetectionRadius);
+        }
     }
 }
