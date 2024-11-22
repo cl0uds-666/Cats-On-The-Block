519cde67b16e692d191cc41537b717a30b72114d
Adam-Bolton-23827742 <adam.bolton23@my.northampton.ac.uk>
2024-11-22 21:12:14 +0000

    finished mission 2

--- Assets/Scripts/Adam/Missions/Mission2/GunPIckUp.cs
+++ Assets/Scripts/Adam/Missions/Mission2/GunPIckUp.cs
@@ -3,19 +3,23 @@ using UnityEngine;
 public class GunPIckUp : MonoBehaviour
 {
     private GameObject Player;
+    private GameObject GunPosition;
 
     private void Start()
     {
         Player = GameObject.Find("Player");
+        GunPosition = GameObject.Find("GunPosition");
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        
         if (Player.GetComponent<Missions>().Mission == 2 && other.CompareTag("Player"))
         {
-            print("gun");
             Player.GetComponent<WeaponSwitching>().weapons.Add(gameObject);
+            transform.parent = Player.transform;
+            transform.position = GunPosition.transform.position;
+            transform.localRotation = Quaternion.Euler(transform.localPosition.x, 90, transform.localPosition.z);
+            Player.GetComponent<Missions>().SelectMission();
         }
     }
 }
