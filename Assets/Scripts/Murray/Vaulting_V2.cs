3128cb454a664ac85384821d6d9da42dd04d1fa6
MurrayRidgway122 <murray.ridgway23@my.northampton.ac.uk>
2024-11-23 20:37:39 +0000

    fixed the code
    so the player didn't collide with the cover when vaulting. Murray

--- Assets/Scripts/Murray/Vaulting_V2.cs
+++ Assets/Scripts/Murray/Vaulting_V2.cs
@@ -1,58 +1,66 @@
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Vaulting_V2 : MonoBehaviour
 {
     public GameObject Player;
-    
-
+    private Collider playerCollider;
     private bool touching;
+    private bool isMoving;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+
     void Start()
     {
-        
+        if (Player != null)
+        {
+            playerCollider = Player.GetComponent<Collider>();
+        }
     }
-
     // Update is called once per frame
     void Update()
     {
-        if (touching == true)
+        if (touching && Player != null && !isMoving)
         {
-            Vector3 Current_Position = Player.transform.position;
-            Vector3 New_Position = new Vector3(Current_Position.x, Current_Position.y, Current_Position.z + 2);
-            if (Input.GetKeyDown(KeyCode.Space))
+            if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.Joystick1Button0))
             {
-                Player.transform.position = New_Position;
-            }
-            else if (Input.GetKeyUp(KeyCode.Joystick1Button0))
-            {
-                Player.transform.position = New_Position;
+                Vector3 Current_Position = Player.transform.position;
+                Vector3 New_Position = new Vector3(Current_Position.x, Current_Position.y, Current_Position.z + 2);
+
+                StartCoroutine(MovePlayerAndEnableCollider(New_Position));
             }
         }
-        
     }
+    // Temporarily disables the player's collider, moves them to the target position, waits briefly, and then re-enables the collider.
+    System.Collections.IEnumerator MovePlayerAndEnableCollider(Vector3 targetPosition)
+    {
+        isMoving = true;
 
-    //private void OnTriggerEnter(Collider other)
-    //{
-    //    if (other.tag == "Trigger")
-    //    {
-    //        touching = true;
-    //    }
+        if (playerCollider != null)
+        {
+            playerCollider.enabled = false;
+        }
 
-    //}
+        Player.transform.position = targetPosition;
 
-    //private void OnTriggerExit(Collider other)
-    //{
-    //    if(other.tag == "Trigger")
-    //    {
-    //        touching = false;
-    //    }
-    //}
+        yield return new WaitForSeconds(0.1f);
 
+        if (playerCollider != null)
+        {
+            playerCollider.enabled = true;
+        }
+
+        isMoving = false;
+    }
+    // Detects when the Player collides with a "Cover" object, enabling interaction and storing references, 
+    // and clears those references when the collision ends.
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.tag == "Cover")
         {
+            Player = collision.gameObject;
+            playerCollider = Player.GetComponent<Collider>();
             touching = true;
+             
         }
     }
 
@@ -60,7 +68,10 @@ public class Vaulting_V2 : MonoBehaviour
     {
         if (collision.gameObject.tag == "Cover")
         {
+            
             touching = false;
+            Player = null;
+            playerCollider.enabled = false;
         }
     }
 }
