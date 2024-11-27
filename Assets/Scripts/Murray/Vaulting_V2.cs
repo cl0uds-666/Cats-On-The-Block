3dca8a32ef4a76ea20ab9aa71eec7e1e6bdcca1c
MurrayRidgway122 <149398092+MurrayRidgway122@users.noreply.github.com>
2024-11-27 11:49:21 +0000

    Vault code works
    to implement Look at short Cover 7 were it as been implemented.

--- Assets/Scripts/Murray/Vaulting_V2.cs
+++ Assets/Scripts/Murray/Vaulting_V2.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class Vaulting_V2 : MonoBehaviour
 {
+    public float Vaulting_Speed;
+    public Rigidbody rb;
     public GameObject Player;
     private Collider playerCollider;
     private bool touching;
@@ -12,19 +14,24 @@ public class Vaulting_V2 : MonoBehaviour
     {
         if (Player != null)
         {
-            playerCollider = Player.GetComponent<CapsuleCollider>();
+            //playerCollider = Player.GetComponent<CapsuleCollider>();
+            rb = Player.GetComponent<Rigidbody>();
+            
         }
     }
     // Update is called once per frame
     void Update()
     {
+        
         if (touching && Player != null && !isMoving)
         {
+            
+            print("working");
             if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.Joystick1Button0))
             {
                 Vector3 Current_Position = Player.transform.position;
-                Vector3 New_Position = new Vector3(Current_Position.x, Current_Position.y, Current_Position.z + 2);
-
+                Vector3 New_Position = new Vector3(Current_Position.x , Current_Position.y , Current_Position.z + 3);
+                
                 StartCoroutine(MovePlayerAndEnableCollider(New_Position));
             }
         }
@@ -36,40 +43,46 @@ public class Vaulting_V2 : MonoBehaviour
 
         if (playerCollider != null)
         {
-            playerCollider.enabled = false;
+            //playerCollider.enabled = false;
+            //rb.useGravity = false;
         }
+        GetComponent<BoxCollider>().enabled = false;
+        //Player.transform.position = New_Position;
+        Player.GetComponent<Rigidbody>().AddForce(Player.transform.forward * Vaulting_Speed);
 
-        Player.transform.position = New_Position;
+        yield return new WaitForSeconds(0.3f);
 
-        yield return new WaitForSeconds(0.1f);
 
+        GetComponent<BoxCollider>().enabled = true;
         if (playerCollider != null)
         {
-            playerCollider.enabled = true;
+            //playerCollider.enabled = true;
+            //rb.useGravity = true;
         }
 
         isMoving = false;
     }
     // setting vairables and object when the player enter and exits the collison box
-    private void OnCollisionEnter(Collision collision)
+    private void OnTriggerEnter(Collider collision)
     {
-        if (collision.gameObject.tag == "Cover")
+        if (collision.gameObject.tag == "Player" && !touching)
         {
             Player = collision.gameObject;
             playerCollider = Player.GetComponent<CapsuleCollider>();
+            
             touching = true;
              
         }
     }
 
-    private void OnCollisionExit(Collision collision)
+    private void OnTriggerExit(Collider collision)
     {
-        if (collision.gameObject.tag == "Cover")
+        if (collision.gameObject.tag == "Player" && touching == true)
         {
-            
+            GetComponent<BoxCollider>().enabled = true;
             touching = false;
             Player = null;
-            playerCollider.enabled = false;
+            //playerCollider.enabled = false;
         }
     }
 }
