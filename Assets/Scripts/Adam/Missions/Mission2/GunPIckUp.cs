08bc16f2c6b8663ebee441a20f738ce5b419f8c6
HarveyW23 <159131573+HarveyW23@users.noreply.github.com>
2024-11-28 15:21:27 +0000

    update

--- Assets/Scripts/Adam/Missions/Mission2/GunPIckUp.cs
+++ Assets/Scripts/Adam/Missions/Mission2/GunPIckUp.cs
@@ -1,9 +1,12 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GunPIckUp : MonoBehaviour
 {
     private GameObject Player;
     private GameObject GunPosition;
+    public GameObject Wheel;
+    public Sprite PistolSprite;
 
     private void Start()
     {
@@ -22,6 +25,7 @@ public class GunPIckUp : MonoBehaviour
                 transform.position = GunPosition.transform.position;
                 transform.localRotation = Quaternion.Euler(transform.localPosition.x, 90, transform.localPosition.z);
                 Player.GetComponent<Missions>().SelectMission();
+                Wheel.GetComponent<Image>().sprite = PistolSprite;
             }
         }
     }
