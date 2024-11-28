843f6a2bc9246dd65fc84389984730d3c172e96f
charlottex444 <159131744+charlottex444@users.noreply.github.com>
2024-11-28 14:34:28 +0000

    gameover

--- Assets/Main Menu/ButtonApperance.cs
+++ Assets/Main Menu/ButtonApperance.cs
@@ -8,7 +8,7 @@ public class ButtonAppearance : MonoBehaviour {
 
     // Use this for initialization
     void Start () {
-        StartCoroutine(HideAndShow(0.6f) );
+        StartCoroutine(HideAndShow(0.57f) );
     }
 
     // Update is called once per frame
