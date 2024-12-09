7b8dee09c4f55a1fa7791192ffe4c11923e32c29
Adam-Bolton-23827742 <adam.bolton23@my.northampton.ac.uk>
2024-12-09 13:43:17 +0000

    changed dialogue skip input

--- Assets/Scripts/Adam/Missions/Mission1/CameraSwitch.cs
+++ Assets/Scripts/Adam/Missions/Mission1/CameraSwitch.cs
@@ -30,7 +30,7 @@ public class CameraSwitch : MonoBehaviour
             SkipButton.SetActive(true);
             NPC.transform.LookAt(new Vector3(Player.transform.position.x, NPC.transform.position.y, Player.transform.position.z));
 
-            if (XInputController.current.bButton.isPressed || Input.GetKeyDown(KeyCode.E))
+            if (XInputController.current.bButton.isPressed || Input.GetKeyDown(KeyCode.Q))
             {
                 SwitchCamera = false;
                 SkipButton.SetActive(false);
